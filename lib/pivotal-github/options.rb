011fabfacab95a53bdf5ed478502962b291f6d6a
Michael Hartl <michael@michaelhartl.com>
2013-02-05 11:31:00 -0800

    Add first real test

--- lib/pivotal-github/options.rb
+++ lib/pivotal-github/options.rb
@@ -1,6 +1,23 @@
+require 'optparse'
+require 'ostruct'
+
 module Options
 
-  def self.foo
-    "bar"
+  def self.parse_known_to(parser, initial_args=ARGV.dup)
+    other_args = []                                     
+    rec_parse = Proc.new { |arg_list|                   
+        begin
+            parser.parse! arg_list                      
+        rescue OptionParser::InvalidOption => e
+            other_args += e.args                        
+            while arg_list[0] && arg_list[0][0,1] != "-"
+                other_args << arg_list.shift            
+            end
+            rec_parse.call arg_list                     
+        end
+    }
+    rec_parse.call initial_args                         
+    other_args                                          
   end
+
 end
--- spec/options/options_spec.rb
+++ spec/options/options_spec.rb
@@ -1,7 +1,28 @@
 require 'spec_helper'
+require 'ostruct'
 
 describe Options do
-  it "should #foo" do
-    Options::foo.should == "bar"
+
+  let(:options) { OpenStruct.new }
+
+  let(:parser) do
+    OptionParser.new do |opts|
+      opts.banner = "Usage: git record [options]"
+      opts.on("-m", "--message MESSAGE",
+              "add a commit message (with ticket #)") do |m| 
+        options.message = m
+      end
+      opts.on("-a", "--all", "commit all changed files") do |a|
+        options.all = a
+      end
+      opts.on("-f", "--finish", "mark story as finished") do |f|
+        options.finish = f
+      end
+      opts.on_tail("-h", "--help", "this usage guide") do
+        puts opts.to_s; exit 0
+      end
+    end    
   end
+  
+  it { should respond_to(:parse_known_to) }
 end
