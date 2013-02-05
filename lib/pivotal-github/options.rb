65c34fea68ed4c8e24aef542290a064e2db7bcd1
Michael Hartl <michael@michaelhartl.com>
2013-02-05 11:49:49 -0800

    Refactor unknown_options

--- lib/pivotal-github/options.rb
+++ lib/pivotal-github/options.rb
@@ -2,21 +2,27 @@ require 'optparse'
 
 module Options
 
-  def self.unknown_options(parser, initial_args=ARGV.dup)
-    other_args = []                                     
-    rec_parse = Proc.new do |arg_list|                   
+  # Returns a list of options unknown to a particular options parser.
+  # For example, if '-a' is a known option but '-b' and '-c' are not,
+  # unknown_options(parser, ['-a', '-b', '-c']) returns ['-b', '-c'].
+  # It also preserves arguments, so 
+  # unknown_options(parser, ['-a', '-b', '-c', 'foo bar']) returns 
+  # ['-b', '-c', 'foo bar'].
+  def self.unknown_options(parser, initial_args)
+    unknown = []                                     
+    recursive_parse = Proc.new do |arg_list|                   
       begin
-          parser.parse! arg_list                      
+        parser.parse! arg_list                      
       rescue OptionParser::InvalidOption => e
-          other_args += e.args                        
-          while arg_list[0] && arg_list[0][0,1] != "-"
-              other_args << arg_list.shift            
-          end
-          rec_parse.call arg_list                     
+        unknown.concat(e.args)
+        while !arg_list.empty? && arg_list.first[0] != "-"
+          unknown << arg_list.shift
+        end
+        recursive_parse.call(arg_list)
       end
     end
-    rec_parse.call initial_args                         
-    other_args                                          
+    recursive_parse.call(initial_args)
+    unknown
   end
 
 end
