d2ba80b3f507e2917e1fc17ad8441b6f0d0393fa
Michael Hartl <michael@michaelhartl.com>
2013-02-05 12:52:34 -0800

    Refactor to use shared examples

--- spec/commands/record_spec.rb
+++ spec/commands/record_spec.rb
@@ -8,10 +8,7 @@ describe Record do
   it { should respond_to(:args) }
   it { should respond_to(:parse) }
 
-  describe "with only known options" do
-    
-    let(:command) { Record.new(['-m', 'message', '-a']) }
-    
+  shared_examples "record with known options" do
     describe "parse" do
       subject { command.parse }
 
@@ -20,18 +17,23 @@ describe Record do
     end
   end
 
+  describe "with only known options" do
+    let(:command) { Record.new(['-m', 'message', '-a']) }
+    it_should_behave_like "record with known options"
+  end
+
+  describe "with a compound argument" do
+    let(:command) { Record.new(['-am', 'message']) }
+    it_should_behave_like "record with known options"
+  end
+
   describe "with some unknown options" do
     let(:command) { Record.new(['-m', 'message', '-a', '-z', '--foo']) }
     
+    it_should_behave_like "record with known options"
+    
     it "should not raise an error" do
       expect { command.parse }.not_to raise_error(OptionParser::InvalidOption)
-    end
-
-    describe "parse" do
-      subject { command.parse }
-
-      its(:message) { should == 'message' }
-      its(:all)     { should be_true }
     end
   end
 end
