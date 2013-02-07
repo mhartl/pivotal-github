61fc44c2ca1aaa163829b027584e2b4e03177da9
Michael Hartl <michael@michaelhartl.com>
2013-02-07 11:42:16 -0800

    [Finishes #43979111] Remove superfluous code

--- lib/pivotal-github.rb
+++ lib/pivotal-github.rb
@@ -3,9 +3,3 @@ require "pivotal-github/options"
 require "pivotal-github/command"
 require "pivotal-github/story_commit"
 require "pivotal-github/story_push"
-
-module Pivotal
-  module Github
-    # Your code goes here...
-  end
-end
