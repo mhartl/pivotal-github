1294607102c0319603c2d3a1e6db792bef447d62
Michael Hartl <michael@michaelhartl.com>
2013-02-07 11:40:49 -0800

    [#43979111] Switch to story_push

--- lib/pivotal-github.rb
+++ lib/pivotal-github.rb
@@ -2,7 +2,7 @@ require "pivotal-github/version"
 require "pivotal-github/options"
 require "pivotal-github/command"
 require "pivotal-github/story_commit"
-require "pivotal-github/submit"
+require "pivotal-github/story_push"
 
 module Pivotal
   module Github
