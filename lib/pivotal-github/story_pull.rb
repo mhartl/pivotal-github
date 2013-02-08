b31f8bee3ca0f90f32e8fe85c327a2ed89fa6e55
Michael Hartl <michael@michaelhartl.com>
2013-02-08 11:08:20 -0800

    [#44109825] Finish basic story-pull

---
+++ lib/pivotal-github/story_pull.rb
@@ -0,0 +1,45 @@
+require 'pivotal-github/command'
+
+class StoryPull < Command
+
+  def parser
+    OptionParser.new do |opts|
+      opts.banner = "Usage: git story-pull [options]"
+      opts.on("-d", "--development BRANCH",
+              "name of development branch (defaults to master)") do |opt|
+        self.options.development = opt
+      end
+      opts.on_tail("-h", "--help", "this usage guide") do
+        puts opts.to_s; exit 0
+      end
+    end
+  end
+
+  # Returns a command appropriate for executing at the command line
+  # For example:
+  #   git checkout master
+  #   git pull
+  #   git checkout <story branch>
+  def cmd
+    lines = ["git checkout #{development_branch}"]
+    c = ['git pull']
+    c << argument_string(unknown_options) unless unknown_options.empty?
+    lines << c.join(' ')
+    lines << ["git checkout #{current_branch}"]
+    lines.join("\n")
+  end
+
+  def run!
+    system cmd
+  end
+
+  private
+
+    def pull_request_branch
+      options.pull_request_branch
+    end
+
+    def development_branch
+      options.development || 'master'
+    end
+end
---
+++ spec/options/story_pull_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+
+describe StoryPull do
+
+  let(:command) { StoryPull.new }
+  before { command.stub(:current_branch).and_return('6283185-tau-manifesto') }
+  subject { command }
+
+  its(:cmd) { should =~ /git pull/ }
+
+  shared_examples "story-pull with known options" do
+    subject { command }
+    it "should not raise an error" do
+      expect { command.parse }.not_to raise_error(OptionParser::InvalidOption)
+    end
+  end
+
+  describe "with no options" do
+    its(:cmd) { should =~ /git checkout master/ }
+    its(:cmd) { should =~ /git pull/ }
+    its(:cmd) { should =~ /git checkout #{command.current_branch}/ }
+  end
+
+  describe "with a target option" do
+    let(:command) { StoryPull.new(['-d', 'develop']) }
+    its(:cmd) { should =~ /git checkout develop/ }
+  end
+
+  describe "with some unknown options" do
+    let(:command) { StoryPull.new(['-d', 'develop', '-a', '-z', '--foo']) }
+    it_should_behave_like "story-pull with known options"
+    its(:cmd) { should =~ /git pull -a -z --foo/ }
+  end
+
+  # describe "command-line command" do
+  #   subject { `bin/git-story-pull --debug -z -t heroku` }
+  #   it { should =~ /git pull -z heroku/ }
+  # end
+end
