2bd14c95d9b284bee9840406a23dd730ab365829
Michael Hartl <michael@michaelhartl.com>
2013-06-18 15:06:33 -0700

    Get accept! working
    [#51928209]

--- lib/pivotal-github/story_accept.rb
+++ lib/pivotal-github/story_accept.rb
@@ -1,11 +1,19 @@
 require 'pivotal-github/command'
 require 'git'
+require 'net/http'
+require 'uri'
 
 class StoryAccept < Command
 
   def parser
     OptionParser.new do |opts|
       opts.banner = "Usage: git story-accept"
+      opts.on("-o", "--override", "override master branch requirement") do |opt|
+        self.options.override = opt
+      end
+      opts.on("-a", "--all", "process all stories") do |opt|
+        self.options.all = opt
+      end
       opts.on_tail("-h", "--help", "this usage guide") do
         puts opts.to_s; exit 0
       end
@@ -21,14 +29,50 @@ class StoryAccept < Command
     delivered = messages.join.scan(delivered_regex).flatten
     delivered.inject([]) do |ids, element|
       ids.concat(element.scan(/[0-9]{8,}/).flatten)
-      ids
+      ids.uniq
+    end
+  end
+
+  def api_token
+    api_filename = '.api_token'
+    if File.exist?(api_filename)
+      @api_token ||= File.read(api_filename).strip
+    else
+      puts "Please create a file called '#{api_filename}'"
+      puts "containing your Pivotal Tracker API token."
+      exit 1
+    end
+  end
+
+  def project_id
+    project_id_filename = '.project_id'
+    if File.exist?(project_id_filename)
+      @project_id_filename ||= File.read(project_id_filename)
+    else
+      puts "Please create a file called '#{project_id}'"
+      puts "containing the Pivotal Tracker project number."
+      exit 1
+    end
+  end
+
+  # Changes a story's state to **Accepted**.
+  def accept!(story_id)
+    api = 'http://www.pivotaltracker.com/services/v3'
+    story_uri = URI.parse("#{api}/projects/#{project_id}/stories/#{story_id}")
+    accepted = "<story><current_state>accepted</current_state></story>"
+    data =  { 'X-TrackerToken' => api_token,
+              'Content-type' => "application/xml" }
+    Net::HTTP.start(story_uri.host, story_uri.port) do |http|
+      http.put(story_uri.path, accepted, data)
     end
   end
 
   def run!
-    # Get Pivotal API key, if necessary
-    # Iterate through the delivered story ids, marking as accepted
+    if story_branch != 'master' && !options['override']
+      puts "Runs only on the master branch by default"
+      puts "Use --override to override"
+      exit 1
+    end
+    ids_to_accept.each { |id| accept!(id) }
   end
-
-  private
 end
