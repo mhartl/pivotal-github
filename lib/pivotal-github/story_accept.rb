9afb0e49d72b1d35f22b59180db1b5fdf5137cf2
Michael Hartl <michael@michaelhartl.com>
2013-07-04 20:25:33 -0700

    Change to faster log grepping for delivered ids
    [#52890295]

--- lib/pivotal-github/story_accept.rb
+++ lib/pivotal-github/story_accept.rb
@@ -1,5 +1,4 @@
 require 'pivotal-github/command'
-require 'git'
 require 'net/http'
 require 'uri'
 require 'nokogiri'
@@ -36,9 +35,9 @@ class StoryAccept < Command
   # These ids are of the form [Delivers #<story id>] or
   # [Delivers #<story id> #<another story id>].
   def ids_to_accept
-    n_commits = `git rev-list HEAD --count`
-    Git.open('.').log(n_commits).inject([]) do |accept, commit|
-      delivered_ids(commit.message).each do |commit_id|
+    delivered_lines = `git log | egrep '\\[Deliver(s|ed) #'`.split("\n")
+    delivered_lines.inject([]) do |accept, line|
+      delivered_ids(line).each do |commit_id|
         return accept if already_accepted?(commit_id) && !options.all
         accept << commit_id
         accept.uniq!
